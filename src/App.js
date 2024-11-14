5b5e79bc3dc7988c4c4ca2a6c807e3410cf1b9e9
Zekeriyaev Danil <zekeriyaev_d@auca.kg>
2024-11-14 10:43:44 +0600

    Created images with doomcat like

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import { ProductCard } from "./components/ProductCard";
 
+const products = [
+  {
+    id:0,
+    name: "Product 1",
+    price: 120,
+    category: "Category 1",
+    image: "https://ih1.redbubble.net/image.5362116032.1700/tst,small,507x507-pad,600x600,f8f8f8.jpg"
+  },
+  {
+    id:1,
+    name: "Product 1",
+    price:1000,
+    category: "Category 1",
+    image: "https://ih1.redbubble.net/image.5362116032.1700/tst,small,507x507-pad,600x600,f8f8f8.jpg"
+  }
+]
+
 function App() {
   return (
-    <div><ProductCard/></div>
+    <div>
+      {
+        products.map((product) => (
+        <ProductCard data={product} />
+      ))}
+    </div>
   );
 }
 
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,3 +1,14 @@
-export function ProductCard() {
-    return <div>Product Card</div>
+import "./index.css"
+
+export function ProductCard(props) {
+    const { image, category, name, price} = props.data;
+
+    return (
+        <div className="product-card">
+            <img width={200} src={image} alt="" />
+            <div>{category}</div>
+            <h4>{name}</h4>
+            <div>{price}</div>
+        </div>
+    )
 }
